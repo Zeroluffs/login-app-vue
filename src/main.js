331e54e8edd0c7a9873a9a86cf51c100707de941
zeroluffs <zeroluffs@gmail.com>
2023-06-01 18:36:01 -0500

    auth-header

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
+import { router } from './router';
+import VeeValidate from 'vee-validate';
+import store from './store';
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
